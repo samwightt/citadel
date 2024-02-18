382bbc3076bda5c072def33ac656fd92288c0861
Sam Wight <me@samw.dev>
2024-02-18 12:06:52 -0600

    Refactor account page components

--- src/routes/accounts/$accountId.lazy.tsx
+++ src/routes/accounts/$accountId.lazy.tsx
@@ -15,7 +15,34 @@ function Account() {
   const { account } = useLoaderData({
     from: "/accounts/$accountId",
   });
-  const { invalidate } = useRouter();
+
+  return (
+    <div className="p-8">
+      <div className="flex flex-row items-center gap-4">
+        <Avatar size="lg" img={account.account.avatar} />
+        <div>
+          <h1 className="text-2xl">{account.account.displayName}</h1>
+          <h2 className="text-xl">
+            @{account.username}@{account.domain}
+          </h2>
+        </div>
+      </div>
+      <div>
+        <p>Followers: {account.account.followersCount}</p>
+        <p>Following: {account.account.followingCount}</p>
+        <p>Created at: {account.account.createdAt}</p>
+        <p>Bio: {JSON.stringify(account.account.fields)}</p>
+      </div>
+      <SuspendButton />
+      <Posts />
+    </div>
+  );
+}
+
+const Posts = () => {
+  const { account } = useLoaderData({
+    from: "/accounts/$accountId",
+  });
   const query = useQuery({
     queryFn: async () => {
       const statuses = await masto()
@@ -28,7 +55,30 @@ function Account() {
     },
     queryKey: ["statuses", account.id],
   });
-  const { mutate, isPending, isSuccess } = useMutation({
+
+  return (
+    <div>
+      <h2 className="text-xl">Posts</h2>
+      {query.data?.map((x) => {
+        return (
+          <div className="border border-gray-100 shadow-lg my-4 rounded-lg p-4">
+            <div dangerouslySetInnerHTML={{ __html: x.content }} />
+            {x.mediaAttachments.map(
+              (x) => x.type === "image" && x.url && <img src={x.url} />
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const SuspendButton = () => {
+  const { account } = useLoaderData({
+    from: "/accounts/$accountId",
+  });
+  const { invalidate } = useRouter();
+  const mutation = useMutation({
     mutationKey: ["suspend", account.id],
     mutationFn: async () => {
       const report = await masto().v1.reports.create({
@@ -50,46 +100,12 @@ function Account() {
   });
 
   return (
-    <div className="p-8">
-      <div className="flex flex-row items-center gap-4">
-        <Avatar size="lg" img={account.account.avatar} />
-        <div>
-          <h1 className="text-2xl">{account.account.displayName}</h1>
-          <h2 className="text-xl">
-            @{account.username}@{account.domain}
-          </h2>
-        </div>
-      </div>
-      <div>
-        <p>Followers: {account.account.followersCount}</p>
-        <p>Following: {account.account.followingCount}</p>
-        <p>Created at: {account.account.createdAt}</p>
-        <p>Bio: {JSON.stringify(account.account.fields)}</p>
-      </div>
-      {account.suspended}
-      <Button
-        className="mt-4"
-        color={account.suspended || isSuccess ? "primary" : "failure"}
-        onClick={() => mutate()}
-        disabled={isPending || account.suspended || isSuccess}
-      >
-        {account.suspended || isSuccess ? "Suspended!" : "Suspend Spam"}
-      </Button>
-      {query.data && (
-        <>
-          <h2 className="text-xl">Posts</h2>
-          {query.data?.map((x) => {
-            return (
-              <div className="border border-gray-100 shadow-lg my-4 rounded-lg p-4">
-                <div dangerouslySetInnerHTML={{ __html: x.content }} />
-                {x.mediaAttachments.map(
-                  (x) => x.type === "image" && x.url && <img src={x.url} />
-                )}
-              </div>
-            );
-          })}
-        </>
-      )}
-    </div>
+    <Button
+      color={account.suspended || mutation.isSuccess ? "primary" : "failure"}
+      onClick={() => mutation.mutate()}
+      disabled={mutation.isPending || account.suspended || mutation.isSuccess}
+    >
+      {account.suspended || mutation.isSuccess ? "Suspended!" : "Suspend Spam"}
+    </Button>
   );
-}
+};
